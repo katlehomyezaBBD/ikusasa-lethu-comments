6e146012d0162ff6fae113363e359d2f958eb81e
Katleho Myeza <katleho.myeza@bbd.co.za>
2025-09-26 21:19:48 +0200

    fixed port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const rateLimit = require('express-rate-limit');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const DATA_DIR = './data/comments';
 
 // Middleware
